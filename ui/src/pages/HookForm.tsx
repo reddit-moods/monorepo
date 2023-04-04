31dc1d01c3806d3eca0b69d1ba78e1f6e0ed4796
Joseph Chen <jchen42703@gmail.com>
2023-04-04 00:33:25 -0400

    feat: properly fetches sentiment and displays it on submit

--- ui/src/pages/HookForm.tsx
+++ ui/src/pages/HookForm.tsx
@@ -6,9 +6,15 @@ import {
   Input,
   Button,
 } from "@chakra-ui/react";
+import { useState } from "react";
 
 interface FormValues {
   name: string;
+}
+
+interface SentimentState {
+  sentiment: "Negative" | "Neutral" | "Positive";
+  confidence: number;
 }
 
 export default function HookForm() {
@@ -18,14 +24,11 @@ export default function HookForm() {
     formState: { errors, isSubmitting },
   } = useForm<FormValues>();
 
+  const [sentimentState, setSentimentState] = useState<
+    SentimentState | undefined
+  >(undefined);
+
   async function onSubmit(values: FormValues) {
-    // return new Promise<void>((resolve) => {
-    //   setTimeout(() => {
-    //     alert(JSON.stringify(values, null, 2));
-    //     resolve();
-    //   }, 3000);
-    // });
-    console.log("values: ", values);
     const API_URL =
       "https://28gsbggq1f.execute-api.us-east-1.amazonaws.com/prod";
     const resp = await fetch(
@@ -37,33 +40,67 @@ export default function HookForm() {
       {
         headers: {
           "content-type": "application/json",
-          mode: "no-cors",
         },
         method: "GET",
+        mode: "cors",
       }
     );
     console.log("resp: ", resp);
+
+    const respBody = (await resp.json()) as SentimentState;
+    console.log(
+      "sentiment: ",
+      respBody.sentiment,
+      "confidence: ",
+      respBody.confidence
+    );
+
+    const state = {
+      sentiment: respBody.sentiment,
+      confidence: respBody.confidence,
+    };
+
+    setSentimentState(state);
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <FormControl isInvalid={errors.name != undefined}>
-        <FormLabel htmlFor="name">Subreddit</FormLabel>
-        <Input
-          id="name"
-          placeholder="name"
-          {...register("name", {
-            required: "This is required",
-            minLength: { value: 1, message: "Minimum length should be 4" },
-          })}
-        />
-        <FormErrorMessage>
-          <>{errors.name && errors.name.message}</>
-        </FormErrorMessage>
-      </FormControl>
-      <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">
-        Submit
-      </Button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl isInvalid={errors.name != undefined}>
+          <FormLabel htmlFor="name">Subreddit</FormLabel>
+          <Input
+            id="name"
+            placeholder="name"
+            {...register("name", {
+              required: "This is required",
+              minLength: { value: 1, message: "Minimum length should be 4" },
+            })}
+          />
+          <FormErrorMessage>
+            <>{errors.name && errors.name.message}</>
+          </FormErrorMessage>
+        </FormControl>
+        <Button
+          mt={4}
+          colorScheme="teal"
+          isLoading={isSubmitting}
+          type="submit"
+        >
+          Submit
+        </Button>
+      </form>
+      <div>
+        {sentimentState ? (
+          <>
+            <p>Sentiment: {sentimentState.sentiment}</p>
+            <p>Confidence: {sentimentState.confidence}</p>
+          </>
+        ) : (
+          <>
+            <p>Please submit a subreddit to get its current sentiment!</p>
+          </>
+        )}
+      </div>
+    </>
   );
 }
