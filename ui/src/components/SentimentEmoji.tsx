8ebf25f8ed0bbcbb838f54b35ab1123967b951fc
Sarah J. Kugelmas <kugelmas.sarah@gmail.com>
2023-04-24 18:41:48 -0400

    minor aesthetic changes
    updated sentiment analysis result emojis and arrangement of result and confidence score

--- ui/src/components/SentimentEmoji.tsx
+++ ui/src/components/SentimentEmoji.tsx
@@ -6,20 +6,20 @@ export default function SentimentEmoji({
   if (sentiment == "Positive") {
     return (
       <>
-        <h1>THis shit is positive</h1>
+        <h1>Sentiment Result: POSITIVE</h1>
       </>
     );
   }
   if (sentiment == "Neutral") {
     return (
       <>
-        <h1>THis shit is neutral</h1>
+        <h1>Sentiment Result: NEUTRAL</h1>
       </>
     );
   }
   return (
     <>
-      <h1>THis shit is negative</h1>
+      <h1>Sentiment Result: NEGATIVE</h1>
     </>
   );
 }
--- ui/src/components/SentimentResult.tsx
+++ ui/src/components/SentimentResult.tsx
@@ -2,6 +2,8 @@ import { Flex } from "@chakra-ui/react";
 import { SentimentState } from "./SubredditSentimentForm";
 import Image from "next/image";
 import HappyEmoji from "../assets/happy.png";
+import NeutralEmoji from "../assets/neutral.png";
+import SadEmoji from "../assets/sad.png";
 import SentimentEmoji from "./SentimentEmoji";
 
 export default function SentimentResult({
@@ -17,16 +19,6 @@ export default function SentimentResult({
         alignItems={"center"}
         flexDirection={"column"}
       >
-        {sentimentState ? (
-          <>
-            <p>Sentiment: {sentimentState.sentiment}</p>
-            <p>Confidence: {sentimentState.confidence}</p>
-          </>
-        ) : (
-          <>
-            <p>Please submit a subreddit to get its current sentiment!</p>
-          </>
-        )}
         {sentimentState && (
           <SentimentEmoji
             sentiment={sentimentState?.sentiment}
@@ -36,6 +28,24 @@ export default function SentimentResult({
         {sentimentState && sentimentState.sentiment == "Positive" && (
           <Image src={HappyEmoji} alt="Sentiment: Happy"></Image>
         )}
+        {sentimentState && sentimentState.sentiment == "Neutral" && (
+          <Image src={NeutralEmoji} alt="Sentiment: Neutral"></Image>
+        )}
+        {sentimentState && sentimentState.sentiment == "Negative" && (
+          <Image src={SadEmoji} alt="Sentiment: Sad"></Image>
+        )}
+
+        {sentimentState ? (
+          <>
+            {/* <p>Sentiment: {sentimentState.sentiment}</p> */}
+            {<p>Confidence: {sentimentState.confidence}</p>}
+          </>
+        ) : (
+          <>
+            <p>Please submit a subreddit to get its current sentiment!</p>
+          </>
+        )}
+
       </Flex>
     </>
   );
