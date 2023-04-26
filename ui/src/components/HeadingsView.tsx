d91aba29eb48c7bbc9dc6080d24f58daea3ffbab
Joseph Chen <jchen42703@gmail.com>
2023-04-26 12:52:14 -0400

    feat: set up template for SentimentRow integration

--- ui/src/components/HeadingsView.tsx
+++ ui/src/components/HeadingsView.tsx
@@ -1,15 +1,31 @@
-import { ModelPrediction, RobertaLabels } from "./SubredditSentimentForm";
+import { ModelPrediction } from "./SubredditSentimentForm";
 
 export default function HeadingsView({
   headings,
   predictions,
 }: {
   headings: string[];
-  predictions: ModelPrediction;
+  predictions: ModelPrediction[];
 }) {
+  const headingsAndPred = headings.map(function (heading: string, i: number) {
+    return {
+      heading,
+      prediction: predictions[i],
+    };
+  });
   return (
     <>
       {/* Render three separate scrollable cards for positive/neutral/negative */}
+      {headingsAndPred.forEach(({ heading, prediction }) => {
+        return (
+          <>
+            <SentimentRow
+              heading={heading}
+              prediction={prediction}
+            ></SentimentRow>
+          </>
+        );
+      })}
     </>
   );
 }
